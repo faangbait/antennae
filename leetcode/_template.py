40ab36d5ce9f9a1bc4c90e8fa32b7835873c799e
S Smith <root@madeof.glass>
2022-04-08 03:31:57 -0500

    Profiling
    Added cProfile wrapper

--- leetcode/_template.py
+++ leetcode/_template.py
@@ -7,6 +7,18 @@ import logging
 import datetime
 from typing import List
 logger = logging.getLogger(__name__)
+import cProfile
+
+def profileit(func):
+    def wrapper(*args, **kwargs):
+        datafn = func.__name__ + ".profile" # Name the data file sensibly
+        prof = cProfile.Profile()
+        retval = prof.runcall(func, *args, **kwargs)
+        prof.dump_stats(datafn)
+        return retval
+
+    return wrapper
+
 
 ################################################################################
 
