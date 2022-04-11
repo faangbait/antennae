99fd62e35a1591d818abcbd0fd48f3e2af7cef33
S Smith <root@madeof.glass>
2022-04-11 03:08:38 -0500

    412 Bonus: Fizzless Conbuzzency
    Doesn't actually pass tests because Leetcode won't let you run 1000+
    threads, but should be the fastest implementation of fizzbuzz in rust.

--- leetcode/lc_412/src/main.rs
+++ leetcode/lc_412/src/main.rs
@@ -1,19 +1,29 @@
 pub struct Solution {}
 
 fn main() {
+    Solution::fizz_buzz(60);
 }
 
+use std::thread;
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        (1..n+1).map(|x| { match x % 15 {
-            0 => "FizzBuzz".to_string(),
-            3 => "Fizz".to_string(),
-            5 => "Buzz".to_string(),
-            6 => "Fizz".to_string(),
-            9 => "Fizz".to_string(),
-            10=> "Buzz".to_string(),
-            12=> "Fizz".to_string(),
-            _ => x.to_string(),
-        }}).collect()
+        let mut handles = vec![];
+            (1..n+1).for_each(|i| {
+                let handle = thread::spawn(move|| {
+                    match i % 15 {
+                        0 => "FizzBuzz".to_string(),
+                        3 => "Fizz".to_string(),
+                        5 => "Buzz".to_string(),
+                        6 => "Fizz".to_string(),
+                        9 => "Fizz".to_string(),
+                        10=> "Buzz".to_string(),
+                        12=> "Fizz".to_string(),
+                        _ => i.to_string(),
+                    }
+                });
+                handles.push(handle);
+            });
+
+        handles.into_iter().map(|f| f.join().unwrap()).collect()
     }
 }
